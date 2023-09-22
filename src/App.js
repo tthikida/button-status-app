38f865453de1849343151a341b9b87ea4139b95e
Tyler <62820237+tthikida@users.noreply.github.com>
2023-09-21 21:57:32 -0700

    react started on webpage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [status, setStatus] = useState(false);
+
+    useEffect(() => {
+        // Fetch the initial status from MongoDB when the component mounts
+        fetchData().then(r => console.print("fetchData - useEffect"));
+    }, []);
+
+    const toggleStatus = async () => {
+        try {
+            // Toggle the status locally
+            setStatus(!status);
+
+            // Update the status in MongoDB
+            await axios.post('/api/updateStatus', { status: !status });
+        } catch (error) {
+            console.error('Error toggling status:', error);
+        }
+    };
+
+    const fetchData = async () => {
+        try {
+            // Fetch the current status from MongoDB
+            const response = await axios.get('/api/getStatus');
+            setStatus(response.data.status);
+        } catch (error) {
+            console.error('Error fetching status:', error);
+        }
+    };
+
+    return (
+        <div className="App">
+            <h1>Toggle Button</h1>
+            <label>
+                Status: {status ? 'On' : 'Off'}
+                <input type="checkbox" checked={status} onChange={toggleStatus} />
+            </label>
+        </div>
+    );
 }
 
 export default App;
